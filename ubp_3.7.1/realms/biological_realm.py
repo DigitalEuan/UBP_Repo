625aedbbc2833cbb4b1573b721eead885b193f51
DigitalEuan <info@digitaleuan.com>
2025-11-28 16:33:57 +1300

    Update biological_realm.py

--- ubp_3.7.1/realms/biological_realm.py
+++ ubp_3.7.1/realms/biological_realm.py
@@ -1,7 +1,7 @@
 """
-Universal Binary Principle (UBP) Framework v3.7 - Biological Realm
-Author: Euan Craig, New Zealand
-Date: 31 October 2025
+Universal Binary Principle (UBP) Framework v3.7.1 - Biological Realm
+Author: Euan R A Craig, New Zealand
+Date: 28 November 2025
 ================================================================================
 
 This module implements biological realm calculations using UBP 3.4 framework.
