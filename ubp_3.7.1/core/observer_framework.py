ba70c11a69bcc6c206bb3f90b9099a47d9e52f0b
DigitalEuan <info@digitaleuan.com>
2025-11-28 16:24:49 +1300

    Update observer_framework.py

--- ubp_3.7.1/core/observer_framework.py
+++ ubp_3.7.1/core/observer_framework.py
@@ -1,7 +1,7 @@
 """
-Universal Binary Principle (UBP) Framework v3.7 - Observer Framework
+Universal Binary Principle (UBP) Framework v3.7.1 - Observer Framework
 Author: Euan Craig, New Zealand
-Date: 31 October 2025
+Date: 28 November 2025
 ================================================================================
 
 This module implements the Self-Actualizing Observer framework, which reveals
